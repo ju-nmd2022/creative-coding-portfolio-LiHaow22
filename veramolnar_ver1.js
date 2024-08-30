147d449b3224432ca9e18620ad02958ae94a63b5
Linn <hali22ow@student.ju.se>
2024-08-30 15:53:11 +0200

    Vera Molnar attempt

---
+++ veramolnar_ver1.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(600, 600);
+  //frameRate(8);
+}
+
+function draw() {
+  background(255);
+
+  let numberShapes = 8;
+  let size = (8 * width) / 10 / numberShapes;
+  noFill();
+  strokeWeight(0.8);
+
+  let colorChoices = [
+    color("#EB7B4C"),
+    color("#0BA19D"),
+    color("#634D9D"),
+    color("#221B22"),
+    color("#0550A2"),
+  ];
+  let shapeColors = [];
+  for (c = 0; c < numberShapes * numberShapes; c++) {
+    shapeColors.push(random(colorChoices));
+  }
+  for (i = 0; i < 20; i++) {
+    let shapeCount = 0;
+
+    for (y = 0; y < numberShapes; y++) {
+      for (x = 0; x < numberShapes; x++) {
+        push();
+        translate(width / 10 + x * size, height / 10 + y * size);
+        stroke(shapeColors[shapeCount]);
+        quad(
+          random(-20, 20),
+          random(-20, 20),
+          random(size / 2 - 7, size + 7),
+          random(-5, 3),
+          random(size / 2 - 7, size + 7),
+          random(size / 2 - 7, size + 7),
+          random(-5, 3),
+          random(size / 2 - 7, size + 7)
+        );
+        pop();
+        shapeCount++;
+      }
+    }
+  }
+  noLoop();
+}
